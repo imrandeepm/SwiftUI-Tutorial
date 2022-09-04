420ea90f0ba4b1678d26173bf14cce9b3f85f770
randeep <mail4bhakta@gmail.com>
2022-09-04 12:18:52 +0530

    Initial Commit

---
+++ SwiftUI Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI Tutorial
+//
+//  Created by randeep m.  on 04/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
