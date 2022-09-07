d04e7971a3a1f84f78c23cef4fb61493aedbe681
randeep <mail4bhakta@gmail.com>
2022-09-07 10:31:03 +0530

    Initial Commit

---
+++ SwiftUI Tutorial/LoopsTutorial.swift
@@ -0,0 +1,20 @@
+//
+//  LoopsTutorial.swift
+//  SwiftUI Tutorial
+//
+//  Created by randeep m.  on 07/09/22.
+//
+
+import SwiftUI
+
+struct LoopsTutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoopsTutorial_Previews: PreviewProvider {
+    static var previews: some View {
+        LoopsTutorial()
+    }
+}
